c6ed0cf63887ce14c126136719489a90b0c38d81
uiman <uniman.malaysia@gmail.com>
2022-04-02 13:20:33 +0000

    register.html, login.html and forget-password.html are created and linked with each other

--- yekareno/account/urls.py
+++ yekareno/account/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import register, login, forgetPassword
 
+app_name = 'account'
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('forget-password/', forgetPassword, name='forget_password'),
 ]
--- yekareno/account/views.py
+++ yekareno/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    return render(request, 'account/register.html')
+
+
+def login(request):
+    return render(request, 'account/login.html')
+
+
+def forgetPassword(request):
+    return render(request, 'account/forget-password.html')
