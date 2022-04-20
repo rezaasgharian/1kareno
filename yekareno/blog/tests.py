951276854a50fb2094044808b8eb578eacb71f94
uiman <uniman.malaysia@gmail.com>
2022-04-20 19:39:15 +0000

    show articles in article page

--- yekareno/blog/tests.py
+++ yekareno/blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import *
 
 # Create your tests here.
+
+
+class UrlsTests(TestCase):
+    def test_fibonacci_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
+
