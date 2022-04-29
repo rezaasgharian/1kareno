3293ab508107fe426a945272a99105c201ebdf55
uiman <uniman.malaysia@gmail.com>
2022-04-29 14:56:50 +0000

    show articles in article page

--- yekareno/account/views.py
+++ yekareno/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import *
+from .forms import RegisterForm, ProfileUpdateForm, UserUpdateForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
