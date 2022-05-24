bf1a95b4c71ebc2584e1a1ec307177b2784beb77
uniman.malaysia@gmail.com <uniman.malaysia@gmail.com>
2022-05-24 14:54:17 +0430

    Start for testing the project

--- yekareno/blog/tests.py
+++ yekareno/blog/tests.py
@@ -1,12 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import *
+from selenium import webdriver
 
 # Create your tests here.
 
 
-class UrlsTests(TestCase):
-    def test_fibonacci_url_is_resolved(self):
-        url = reverse('index')
-        self.assertEquals(resolve(url).func, index)
+browser = webdriver.chrome()
+browser.get('http://localhost:8000/blog')
+browser.find_element_by_xpath('//*[@id="nav"]/li[5]/a').click()
+
+
+#
+# class FunctionalTestCase(TestCase):
+#     # def test_blog_page(self):
+#         pass
+#
+# class UnitTestCase(TestCase):
+#     pass
+#
+# class UrlsTests(TestCase):
+#     def test_fibonacci_url_is_resolved(self):
+#         url = reverse('index')
+#         self.assertEquals(resolve(url).func, index)
+
 
