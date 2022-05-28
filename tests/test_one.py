19926f0049ddfca491cceb1d109b82bd16929f1d
uniman.malaysia@gmail.com <uniman.malaysia@gmail.com>
2022-05-28 18:25:24 +0430

    Start using pytest library Add fixtures

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,12 +1,22 @@
-# import pytest
-#
-# class TestOne():
-#     def test_1(self):
-#        assert 1 == 1
-#
-#     def test_2(self):
-#         assert 'a' in 'rezoo'
-#
-#     def test_3(self):
-#         assert 5*5 == 25
-#
+import pytest
+
+class TestOne():
+    def test_1(self):
+       assert 1 == 1
+
+    # @pytest.mark.uniman
+    def test_2(self):
+        assert 'a' in 'rezoo'
+
+    def test_3(self):
+        assert 5*5 == 25
+
+@pytest.fixture()
+def fixture_1():
+    return 1
+
+def test_fixture_1(fixture_1):
+    num = fixture_1
+    assert 1 == num
+
+
--- yekareno/blog/tests.py
+++ yekareno/blog/tests.py
@@ -1,9 +1,9 @@
-import pytest
-
-class TestOne():
-    def test_1(self):
-       assert 1 == 1
-
-    def test_2(self):
-        assert 'a' in 'rezoo'
-
+# import pytest
+#
+# class TestOne():
+#     def test_1(self):
+#        assert 1 == 1
+#
+#     def test_2(self):
+#         assert 'a' in 'rezoo'
+#
