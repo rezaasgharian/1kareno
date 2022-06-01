8654ea2e6e6d7f44d6e3455e054c3f90ec2de042
uniman.malaysia@gmail.com <uniman.malaysia@gmail.com>
2022-06-01 21:44:23 +0430

    Wrote a pytest code to connect to the User model

--- conftest.py
+++ conftest.py
@@ -3,14 +3,42 @@ from django.contrib.auth.models import User
 from blog.models import Article
 
 
-# @pytest.fixture()
-# def user_1(db):
-#     user = User.objects.create_user('unim')
-#     return user
+# def create_article():
+@pytest.fixture
+def new_user_factory(db):
+    def create_user(
+            username: str = 'username',
+            password: str = None,
+            first_name: str = 'firstname',
+            last_name: str = 'lastname',
+            email: str = 'test@gmail.com',
+            is_staff: str = False,
+            is_superuser: str = False,
+            is_active: str = True,
+    ):
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            is_active=is_active,
+        )
+        return user
+    return create_user()
+
 
 @pytest.fixture()
-def user_1(db):
-    return User.objects.create_user('Reza', 'reza@gmil.com')
+def new_user(db, new_user_factory):
+    return new_user_factory('uniman', '1216', 'Reza')
+
+
+# @pytest.fixture()
+# def user_1(db):
+#     user1 = User.objects.create_user('Reza', 'reza@gmil.com')
+#     return user1
 
 
 @pytest.fixture()
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,19 +3,27 @@ from django.contrib.auth.models import User
 from blog.models import *
 
 
-def test_check_password(user_1):
-    user_1.set_password('1216')
-    print(user_1.password)
-    assert user_1.check_password('1216') is True
+# def test_check_password(user_1):
+#     user_1.set_password('1216')
+#     print(user_1.password)
+#     assert user_1.check_password('1216') is True
+#
+#
+# def test_check_email(user_1):
+#     print(user_1.email)
+#     assert user_1.email == 'reza@gmil.com'
+#
+#
+# def test_username(user_1):
+#     assert user_1.username == 'Reza'
 
 
-def test_check_email(user_1):
-    print(user_1.email)
-    assert user_1.email == 'reza@gmil.com'
+def test_new_user(new_user):
+    assert new_user.username == 'uniman'
 
 
-def test_create_article(article_1):
-    assert article_1.title == 'Django'
+# def test_create_article(article_1):
+#     assert article_1.title == 'Django'
 
 # @pytest.mark.django_db
 # def test_create_user():
